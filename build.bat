813240ad6a8babcf5c1e4d5bf4afa195b6fca166
kunming.xie <kunming.xie@hotmail.com>
2017-06-08 11:40:04 +0800

    fix win 64 build support win64 release build

--- build.bat
+++ build.bat
@@ -2,6 +2,10 @@ rmdir /q /s build
 mkdir build
 xcopy build_config.h build
 cd build
+
+::cmake -G "Visual Studio 10 Win64" ..
+::cmake --build . --config release
+
 cmake -G "Visual Studio 10" ..
 cmake --build .
 if errorlevel 1 (
